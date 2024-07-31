2fad78764585cc08d6b9c583a9f4d19897b53482
Addison Hernandez <addison.hernandez@gmail.com>
2024-07-31 11:45:35 -0400

    Track plugin scripts

---
+++ lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,17 @@
+-- Colorschemes that can be loaded in init.vim
+
+return {
+  -- tokyonight that ships with Kickstart.nvim
+  'folke/tokyonight.nvim',
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+  },
+  'olimorris/onedarkpro.nvim',
+  {
+    'sainnhe/gruvbox-material',
+    -- init = function()
+    --   vim.g.gruvbox_material_foreground = 'original'
+    -- end,
+  },
+}
